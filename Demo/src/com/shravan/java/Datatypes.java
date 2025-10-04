fed91d97ce3b362a27ad57f4ddc07367a626c008
shravan-techie <shravan.devtechie@gmail.com>
2025-10-04 11:09:52 +0530

    practice data type in java

---
+++ Demo/src/com/shravan/java/Datatypes.java
@@ -0,0 +1,10 @@
+package com.shravan.java;
+
+public class Datatypes {
+	public static void main(String[] args) {
+		int no1 = 12;
+		int no2 = 14;
+		int sum = no1 + no2;
+		System.out.println(sum);
+	}
+}
